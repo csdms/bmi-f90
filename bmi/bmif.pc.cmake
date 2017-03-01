14eea1908e0eb73feb2811d3afe092f96c12a334
Mark Piper <mark.piper@colorado.edu>
2017-03-01 09:43:27 -0700

    Use ungenundtized name

--- bmi/bmif.pc.cmake
+++ bmi/bmif.pc.cmake
@@ -1,5 +1,5 @@
 Name: BMI
-Description: Basic Modeling Interface
+Description: Basic Model Interface
 Version: ${BMI_VERSION}
 Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmif
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include
