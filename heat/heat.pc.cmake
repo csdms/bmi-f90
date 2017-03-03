af937b0d4ef8dd31bdcb345f5205a0a1367c0de3
Mark Piper <mark.piper@colorado.edu>
2017-03-03 11:36:39 -0700

    Add build instructions with cmake and example main program

---
+++ heat/heat.pc.cmake
@@ -0,0 +1,5 @@
+Name: heat
+Description: An example of the heat equation in Fortran 90
+Version: ${HEAT_VERSION}
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lheat
+Cflags: -I${CMAKE_INSTALL_PREFIX}/include
