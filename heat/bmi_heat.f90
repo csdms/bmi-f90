5ab836e2012ff4e1cf2ac862b7a7ac3b946d66b7
Mark Piper <mark.piper@colorado.edu>
2017-03-06 12:09:23 -0700

    Add the get_component_name method
    This is one of the simplest BMI methods. I added it first just
    to make sure the build process is working.

--- heat/bmi_heat.f90
+++ heat/bmi_heat.f90
@@ -1,10 +1,21 @@
 module bmi_heat
 
-  use bmif
+  use bmi_params
   use heat
   implicit none
 
   character (len=BMI_MAXCOMPNAMESTR), target :: &
        component_name = "The 2D Heat Equation"
+
+contains
+
+  function get_component_name(self, name) result(status)
+    type (heat_model), intent (in) :: self
+    character (len=*), pointer, intent (out) :: name
+    integer :: status
+
+    name => component_name
+    status = BMI_SUCCESS
+  end function get_component_name
 
 end module bmi_heat
