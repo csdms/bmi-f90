b00785c2eae51fad2e5ab8cd4551d19aa239845b
Mark Piper <mark.piper@colorado.edu>
2017-03-06 16:16:06 -0700

    Add initialize and finalize BMI methods

--- heat/bmi_heat.f90
+++ heat/bmi_heat.f90
@@ -9,6 +9,32 @@ module bmi_heat
 
 contains
 
+  ! Perform startup tasks for the model.
+  function initialize(self, config_file) result(status)
+    type (heat_model), intent (out) :: self
+    character (len=*), intent (in) :: config_file
+    integer :: status
+
+    if (len (config_file) > 0) then
+       call initialize_from_file(self, config_file)
+    else
+       call initialize_from_defaults(self)
+    end if
+
+    status = BMI_SUCCESS
+  end function initialize
+
+  ! Perform tear-down tasks for the model.
+  function finalize(self) result(status)
+    type (heat_model), intent (inout) :: self
+    integer :: status
+
+    call cleanup(self)
+
+    status = BMI_SUCCESS
+  end function finalize
+
+  ! Get the name of the model.
   function get_component_name(self, name) result(status)
     type (heat_model), intent (in) :: self
     character (len=*), pointer, intent (out) :: name
